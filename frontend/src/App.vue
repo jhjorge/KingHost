3f89220f343293a58ab3964cd297555508df9544
João Henrique <jhjorgexs@gmail.com>
2025-07-01 13:50:50 -0300

    project basis

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header class="teste">      
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="sass" scoped>
+.teste 
+  color: red
+  font-weight: bold
+
+</style>
