f1df903de45ea968b6b850992f173a6b1ec4d98b
João Henrique <jhjorgexs@gmail.com>
2025-07-01 18:15:04 -0300

    feat(layout): Implementação do scopo da página inicial

---
+++ frontend/src/components/ui/MovieCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PrimaryButton from "./buttons/PrimaryButton.vue";
+</script>
+<template>
+  <article class="max-w-md shadow-md p-6 rounded-2xl space-y-2">
+    <header>Card title</header>
+    <hr />
+    <main>
+      <p class="line-clamp-3">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id cum eaque fugit fuga
+        blanditiis minus veniam quisquam delectus? Recusandae dolorum cupiditate ullam
+        minima, fugiat nesciunt? Assumenda maxime optio corrupti maiores.
+      </p>
+    </main>
+    <hr />
+    <footer>
+      <PrimaryButton aria-label="veja mais sobre o filme"> Veja mais</PrimaryButton>
+    </footer>
+  </article>
+</template>
+<style scoped lang="scss"></style>
